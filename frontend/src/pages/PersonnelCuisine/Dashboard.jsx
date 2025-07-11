c3eed985a56fcd3b60a245cde3b2aa00f46d4952
Sylvano96 <eliasvano78@gmail.com>
2025-07-11 10:38:59 +0200

    Modification le 11:39 11/07/2025 (farany)

--- frontend/src/pages/PersonnelCuisine/Dashboard.jsx
+++ frontend/src/pages/PersonnelCuisine/Dashboard.jsx
@@ -78,6 +78,7 @@ export default function DashboardpersCuisine() {
     return () => {
       socket.off("updateStatus");
     };
+    
   }, []);
 
   const changeDataBaseStatus = async (id, status) => {
