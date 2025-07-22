06fd5ed1b23250b180274f2c4d83b2d00e22480c
andrea00728 <andrea112samuel@gmail.com>
2025-07-22 07:17:50 +0200

    navigation rol

--- frontend/src/pages/RoleSelector.jsx
+++ frontend/src/pages/RoleSelector.jsx
@@ -19,18 +19,20 @@ export default function RoleSelector() {
   };
 
   return (
-    <div className="flex flex-col items-center gap-6 mt-20">
+    <div className="flex flex-col items-center gap-6 mt-20 bg-gray-100 rounded-lg w-[50%] mx-auto mt-50 p-10  ">
       <h2 className="text-2xl font-bold text-gray-700">Choisissez votre rôle</h2>
       <div className="flex gap-6">
         <button
           onClick={() => choisirRole(user.role)}
-          className="bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700"
+          class="middle none center mr-3 rounded-lg bg-pink-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          data-ripple-light="true"
         >
           Continuer comme {user.role}
         </button>
         <button
           onClick={() => choisirRole("organisateur")}
-          className="bg-green-600 text-white px-6 py-3 rounded-lg hover:bg-green-700"
+           class="middle none center rounded-lg py-3 px-6 font-sans text-xs font-bold uppercase text-pink-500 transition-all hover:bg-pink-500/10 active:bg-pink-500/30 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          data-ripple-dark="true"
         >
           Créer mon propre événement
         </button>
