8908b0427b6d5d9bd692ef7576e591607286ac10
andrea00728 <andrea112samuel@gmail.com>
2025-07-28 13:41:44 +0200

    affichage commentaire

--- frontend/src/services/testimonyService.jsx
+++ frontend/src/services/testimonyService.jsx
@@ -1,4 +1,17 @@
+import { data } from "react-router-dom";
 import axiosClient from "../api/axios-client";
+
+
+
+export  const createTestimony =async (payload,token)=>{
+    const response= await axiosClient.post('/commentaire',payload,{
+        headers:{
+            Authorization:`Bearer ${token}`
+        }
+    });
+    return response.data;
+}
+
 /**
  * 
  * @returns 
@@ -9,3 +22,54 @@ export const findOneRecent =async ()=>{
     const response  = await  axiosClient.get('/commentaire/recent');
     return response.data;
 }
+
+
+/**
+ * 
+ * @returns 
+ * 
+ * recuperation des 3 commentaire recent
+ * 
+ */
+export const findthreeRecent =async ()=>{
+    const response  = await  axiosClient.get('/commentaire/diff-commentaire');
+    return response.data;
+}
+
+
+/**
+ * 
+ * api pour la recuperation  du commentaire recent avant le dernier commentaire
+ *
+ */
+export const findSecondLastCommentaireRecent =async ()=>{
+    const response  = await  axiosClient.get('/commentaire/second-commentaire');
+   return response.data;
+}
+
+
+/**
+ * 
+ * @returns 
+ * 
+ * api pour la recuperation  du troisieme commentaire recent avant les deux dernier commentaire
+ */
+
+export const findThirdLastCommentaireRecent =async ()=>{
+    const response  = await  axiosClient.get('/commentaire/third-commentaire');
+    return response.data;
+}
+
+
+/**
+ * 
+ * @returns 
+ * 
+ * api pour la recuperation  du quatrieme commentaire recent avant les trois dernier commentaire    
+ */
+export const findFourthLastCommentaireRecent =async ()=>{
+    const response  = await  axiosClient.get('/commentaire/fourth-commentaire');
+    return response.data;
+}
+
+
