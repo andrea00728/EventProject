8db6caae289f50348d2dc9121eeaebe963f72430
Sylvano96 <eliasvano78@gmail.com>
2025-08-11 09:08:14 +0300

    Mis à jour 09:08 11/08/2025

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 import * as crypto from 'crypto';
-(global as any).crypto = crypto;
+// (global as any).crypto = crypto;
 
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
