ff10e35d4f05df896827818d85dbaa5ebca9f90c
Safidy Rakotonirina <safidyrktnr@gmail.com>
2025-08-19 09:25:51 +0300

    zay

--- frontend/src/pages/Accueil.jsx
+++ frontend/src/pages/Accueil.jsx
@@ -4,11 +4,8 @@ import { FaAward, FaMapMarkerAlt, FaUsers, FaCalendarAlt, FaClock } from "react-
 import TestimonialsSection from "../util/testimonialsSection";
 import { getAllEvents } from '../services/evenementServ';
 import tutorialVideo from "../assets/demo.mp4"; // import de la vidéo
-<<<<<<< HEAD
 import SalleAvecFormulaire from '../util/demo';
 import Demo from '../util/demo';
-=======
->>>>>>> origin/lioka
 
 export default function Accueil() {
   const [allEvents, setAllEvents] = useState([]);
