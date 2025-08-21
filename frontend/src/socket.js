9aedb65ad666ccfcd05b2b551988e4c3622e38ab
Sylvano96 <eliasvano78@gmail.com>
2025-08-21 16:15:50 +0300

    Mis à jour 16:15 21/08/2025

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,24 +1,28 @@
 import { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 import { getUserIdForToken } from './services/userService';
+import { useStateContext } from './context/ContextProvider';
 
 export const SOCKET_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000';
 
 export function useSocket() {
   const [socket, setSocket] = useState(null);
+  const {token} = useStateContext()
 
   useEffect(() => {
     let newSocket;
 
     async function connectSocket() {
-      const userId = await getUserIdForToken();
-      if (!userId) return;
+      if(token){
+        const userId = await getUserIdForToken();
+        if (!userId) return;
 
-      newSocket = io(SOCKET_URL, {
-        transports: ['websocket'],
-        auth: { userId },
-      });
-      setSocket(newSocket);
+        newSocket = io(SOCKET_URL, {
+          transports: ['websocket'],
+          auth: { userId },
+        });
+        setSocket(newSocket);
+      }
     }
 
     connectSocket();
