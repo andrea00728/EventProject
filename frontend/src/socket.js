33095d0cfeadddb69b3d5b00083f6ba3cb2c120c
lioka <liokabenjamina21@gmail.com>
2025-08-29 13:47:48 +0300

    socket

---
+++ frontend/src/socket.js
@@ -0,0 +1,41 @@
+
+
+import { useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
+import { getUserIdForToken } from './services/userService';
+import { url } from './api/url';
+//***
+// commit
+//  */
+
+
+export function useSocket() {
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    let newSocket;
+
+    async function connectSocket() {
+        const userId = await getUserIdForToken();
+        if (!userId) return;
+
+      newSocket = io(`${url}`, {
+        transports: ['websocket'],
+        auth: { userId },
+      });
+      setSocket(newSocket);
+    }
+
+    connectSocket();
+
+    return () => {
+      if (newSocket) {
+        newSocket.disconnect();
+      }
+    };
+  }, []);
+
+  return socket;
+}
+
+/////
