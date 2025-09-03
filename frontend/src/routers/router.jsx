fb768c32bc04e44e81b8d71f0e366b45f617d70f
Safidy Rakotonirina <safidyrktnr@gmail.com>
2025-09-03 14:05:21 +0300

    dashboard event

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -79,7 +79,7 @@ import PublicLayout from "../layouts/PublicLayout.jsx";
 import EventProtectLayout from "../layouts/eventProtectLayout.jsx";
 import AdminManagementPage from "../pages/Admin/AdminManagmentTable.jsx";
 import DashboardPage from "../pages/dashborad/dashboardEvent.jsx";
-import DashboardEvenement from "../pages/restauration/DashboardEvenement.jsx";
+import DashboardEvenement from "../pages/dashborad/DashboardEvenement.jsx";
 
 const router = createBrowserRouter([
   {
