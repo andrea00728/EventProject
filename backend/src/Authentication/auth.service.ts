e00e6dc5e65f7d0a035440188f7a0be718d213f2
lioka <liokabenjamina21@gmail.com>
2025-09-12 14:42:53 +0300

    dernier maj

--- backend/src/Authentication/auth.service.ts
+++ backend/src/Authentication/auth.service.ts
@@ -49,8 +49,8 @@ export class AuthService {
     this.transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
       },
     });
   }
@@ -630,7 +630,7 @@ export class AuthService {
       await this.userRepository.save(newUser);
 
       const mailOptions = {
-        from: process.env.EMAIL_USER,
+        from: process.env.SMTP_USER,
         to: email,
         subject: 'Vérifiez votre adresse email',
         html: `
